5aa7b30552459d19ce1c5d593899958db1f7c0da
Michael Kölle <michael.koelle@ifi.lmu.de>
2024-01-26 17:12:27 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="marl-aquarium",
-    version="0.1.6",
+    version="0.1.7",
     packages=find_packages(exclude=["examples"]),
     license="MIT",
     description="Aquarium: A Comprehensive Framework for Exploring Predator-Prey Dynamics through Multi-Agent Reinforcement Learning Algorithms",
